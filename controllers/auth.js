147df92d99593e6ed88690759f5238e618d90d0f
calendarApp <angelluisqueso@gmail.com>
2023-06-17 20:56:56 -0400

    bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,34 +3,37 @@ const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
 
 const crearUsuario = async (req, res = response) => {
-  const { email} = req.body;
+  const { email, password } = req.body;
 
   try {
-    let usuario = await Usuario.findOne({ email }); 
+    let usuario = await Usuario.findOne({ email })  ;
 
-    if (usuario) {
+    if (usuario)
+     {
       return res.status(400).json({
         ok: false,
         msg: "un usario ya existe con ese correo ",
       });
+     } 
+    else
+    {
+      usuario = new Usuario(req.body);
+
+      //encryptar password
+      const salt = bcrypt.genSaltSync();
+      usuario.password = bcrypt.hashSync(toString(password), salt);
+
+      await usuario.save();
+
+      res.status(201).json({
+        ok: true,
+        uid: usuario.id,
+        name: usuario.name,
+        password: usuario.password,
+      });
     }
-    else{
-    usuario = new Usuario(req.body);
-
-    //encryptar password 
-      const salt  = bcrypt.genSaltSync(); 
-      usuario.password = bcrypt.hashSync(usuario.password,  salt);
-      console.log(salt);
-     await usuario.save();
-
-    res.status(201).json({
-      ok: true,
-      uid: usuario.id,
-      name:usuario.name,
-      password:usuario.password,
-    });}
-    
-  } catch (error) {
+   } 
+   catch (error) {
     res.status(500).json({
       ok: false,
       msg: "vaya con el administrador",
@@ -38,8 +41,14 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res = response) => {
+
+
+
+
+const loginUsuario = async (req, res = response) => {
   const { email, password } = req.body;
+
+ 
 
   res.status(201).json({
     ok: true,
@@ -48,6 +57,7 @@ const loginUsuario = (req, res = response) => {
     password,
   });
 };
+
 
 const revalidarToken = (req, res = response) => {
   const { email, password } = req.body;
